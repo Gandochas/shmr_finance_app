c45a5fbd4b372a9dec0a95d27eccb991ee1c00ec
Mark Gorelov <mark.gorelov.2018@bk.ru>
2025-06-07 22:22:18 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# shmr_finance_app
+
+Finance app as first part of Yandex Summer School for mobile developers
---
+++ android/app/src/main/kotlin/com/example/shmr_finance_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shmr_finance_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
