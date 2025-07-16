f87d02a3ceebe15a7325e1a1b7d31f6121c58d15
Mark Gorelov <mark.gorelov.2018@bk.ru>
2025-07-17 00:24:09 +0300

    implemented settings page

--- lib/presentation/pages/settings_page.dart
+++ lib/presentation/pages/settings_page.dart
@@ -5,16 +5,75 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-        title: const Text(
-          'Настройки',
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400),
-        ),
+        backgroundColor: theme.appBarTheme.backgroundColor,
+        title: Text('Настройки', style: theme.appBarTheme.titleTextStyle),
         centerTitle: true,
       ),
-      body: const Center(child: Text('Settings')),
+      body: Column(
+        children: [
+          DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: theme.dividerColor)),
+            ),
+            child: ListTile(
+              title: Text('Системная тема', style: theme.textTheme.bodyLarge),
+              trailing: Switch.adaptive(value: false, onChanged: (value) {}),
+            ),
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: theme.dividerColor)),
+            ),
+            child: ListTile(
+              title: Text('Основной цвет', style: theme.textTheme.bodyLarge),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.navigate_next),
+              ),
+            ),
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: theme.dividerColor)),
+            ),
+            child: ListTile(
+              title: Text('Хаптики', style: theme.textTheme.bodyLarge),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.navigate_next),
+              ),
+            ),
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: theme.dividerColor)),
+            ),
+            child: ListTile(
+              title: Text('Код пароль', style: theme.textTheme.bodyLarge),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.navigate_next),
+              ),
+            ),
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: theme.dividerColor)),
+            ),
+            child: ListTile(
+              title: Text('Язык', style: theme.textTheme.bodyLarge),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.navigate_next),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
